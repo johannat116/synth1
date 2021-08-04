bebd66c9d1eed86e23f42689c474dababfd0b304
johannat116 <johannat116@gmail.com>
2021-08-03 23:36:12 -0500

    8/3/21

--- synth.js
+++ synth.js
@@ -1,12 +1,40 @@
+// const triggerkey= document.querySelectorAll('.keyboard') 
 
-var synth = new Tone.Synth().toMaster();
+// triggerKey.forEach(keyboard => {
+//     keyboard.addEventListener('click', () => triggerNote (key))
+// })
 
-var notes =[ 'c','d','e','f']
+
+// function triggerNote(keyboard){
+//     const noteaudio= document.getElementById (keyboard.dataset.note)
+//     noteAudio.play()
+// }
 
 
-for (var i=0; i< notes.length; i++){
-    var note = notes[i];
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.whitekey')
+const blackKeys = document.querySelectorAll('.key.blackkey')
+
+keys.forEach(key => {
+  key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+  if (e.repeat) return
+  const key = e.key
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+  const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
+
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note)
+  noteAudio.currentTime = 0
+  noteAudio.play()
+  key.classList.add('active')
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+  })
 }
-
-
-document.getElementById
